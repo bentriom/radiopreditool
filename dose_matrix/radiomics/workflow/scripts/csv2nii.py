45fd4dd246353da4cbbe25b3bd12150223413041
Mahmoud Bentriou <mahmoud.bentriou@centralesupelec.fr>
2021-11-15 23:03:36 +0100

    Handle the empty newdosi files by creating empty files. Radiomics with NaN are correctly created.

--- dose_matrix/radiomics/workflow/scripts/csv2nii.py
+++ dose_matrix/radiomics/workflow/scripts/csv2nii.py
@@ -31,20 +31,20 @@ def check_nan_values(df):
 def to_nii(path_csv, path_nii, list_csv_files, voi_type):
     voi_type = voi_type.upper()
     assert voi_type in ['T', 'NUAGE', 'MATH']
+    relevant_cols = ['X', 'Y', 'Z', voi_type, 'ID2013A']
+    int_cols = ['X', 'Y', 'Z', voi_type]
     # Reads the newdosi files and sums the matrices if interval time <= 3 months
     idx_sort_by_date_csv_files = np.argsort([get_date(newdosi_file) for newdosi_file in list_csv_files])
     first_newdosi_file = list_csv_files[idx_sort_by_date_csv_files[0]]
     df_dosi = pd.read_csv(path_csv + first_newdosi_file)
     df_dosi.columns = df_dosi.columns.str.upper()
+    df_dosi = df_dosi[relevant_cols]
     test_xyz_nan = check_nan_values(df_dosi)
     if test_xyz_nan:
         nbr_rows_before = df_dosi.shape[0]
         df_dosi = df_dosi.dropna(subset = ['X', 'Y', 'Z', 'ID2013A'])
         nbr_rows_after = df_dosi.shape[0]
         print(f"{first_newdosi_file}: has NaN values in X, Y or Z. Dropping {nbr_rows_before - nbr_rows_after} rows.")
-    relevant_cols = ['X', 'Y', 'Z', voi_type, 'ID2013A']
-    int_cols = ['X', 'Y', 'Z', voi_type]
-    df_dosi = df_dosi[relevant_cols]
     ctr_patient, numcent_patient = get_ctr_numcent(first_newdosi_file)
     date_last_treatment = get_date(first_newdosi_file)
     for i in idx_sort_by_date_csv_files[1:]:
@@ -66,6 +66,9 @@ def to_nii(path_csv, path_nii, list_csv_files, voi_type):
                 print(f"{current_newdosi_file}: has NaN values in X, Y or Z. Dropping {nbr_rows_before - nbr_rows_after} rows.")
             if df_dosi.shape[0] != df_other_dosi.shape[0]:
                 print(f"{first_newdosi_file} and {current_newdosi_file}: rows numbers are different. Stopping the sum.")
+                break
+            if df_other_dosi.shape[0] <= 1:
+                print(f"{current_newdosi_file}: has <= 1 rows. Stopping the sum.")
                 break
             well_ordered_rows = df_dosi['X'].equals(df_other_dosi['X']) and \
                                 df_dosi['Y'].equals(df_other_dosi['Y']) and \
@@ -91,26 +94,33 @@ def to_nii(path_csv, path_nii, list_csv_files, voi_type):
         #date_last_treatment = date_treatment
     patient_filename = f"newdosi_{ctr_patient}_{numcent_patient}"
 
-    # Coordinates, labels and doses as 3D arrays
-    x = np.array(df_dosi['X'] - min(df_dosi['X']), dtype='int') // 2
-    y = np.array(df_dosi['Y'] - min(df_dosi['Y']), dtype='int') // 2
-    z = np.array(df_dosi['Z'] - min(df_dosi['Z']), dtype='int') // 2
-    image_size = (max(x)+1,max(y)+1,max(z)+1)
-    labels_3d = np.zeros(image_size)
-    labels_3d[x,y,z] = df_dosi[voi_type]
-    dosi_3d = np.zeros(image_size)
-    dosi_3d[x,y,z] = df_dosi['ID2013A']
-
-    # Save as images
+    # Images settings
     os.makedirs(path_nii, exist_ok=True)
     file_dosi_nii = path_nii + patient_filename + '_ID2013A.nii.gz'
-    image_dosi = sitk.GetImageFromArray(dosi_3d)
-    image_dosi.SetSpacing((2.0,2.0,2.0))
-    image_dosi.SetOrigin((0.0,0.0,0.0))
-    sitk.WriteImage(image_dosi, file_dosi_nii)
     file_mask_nii = path_nii + patient_filename + f"_{voi_type}.nii.gz"
-    image_mask = sitk.GetImageFromArray(labels_3d)
-    image_mask.SetSpacing((2.0,2.0,2.0))
-    image_mask.SetOrigin((0.0,0.0,0.0))
-    sitk.WriteImage(image_mask, file_mask_nii)
+    
+    # If the first newdosi file was empty
+    if df_dosi.shape[0] <= 1:
+        print(f"{first_newdosi_file}: has <= 1 rows. Creating empty nii files.")
+        open(file_dosi_nii, 'w').close()
+        open(file_mask_nii, 'w').close()
+    else:
+        # Coordinates, labels and doses as 3D arrays
+        x = np.array(df_dosi['X'] - min(df_dosi['X']), dtype='int') // 2
+        y = np.array(df_dosi['Y'] - min(df_dosi['Y']), dtype='int') // 2
+        z = np.array(df_dosi['Z'] - min(df_dosi['Z']), dtype='int') // 2
+        image_size = (max(x)+1,max(y)+1,max(z)+1)
+        labels_3d = np.zeros(image_size)
+        labels_3d[x,y,z] = df_dosi[voi_type]
+        dosi_3d = np.zeros(image_size)
+        dosi_3d[x,y,z] = df_dosi['ID2013A']
+        # Save as images
+        image_dosi = sitk.GetImageFromArray(dosi_3d)
+        image_dosi.SetSpacing((2.0,2.0,2.0))
+        image_dosi.SetOrigin((0.0,0.0,0.0))
+        sitk.WriteImage(image_dosi, file_dosi_nii)
+        image_mask = sitk.GetImageFromArray(labels_3d)
+        image_mask.SetSpacing((2.0,2.0,2.0))
+        image_mask.SetOrigin((0.0,0.0,0.0))
+        sitk.WriteImage(image_mask, file_mask_nii)
 
--- dose_matrix/radiomics/workflow/scripts/feature_extractor.py
+++ dose_matrix/radiomics/workflow/scripts/feature_extractor.py
@@ -9,9 +9,6 @@ from radiomics import featureextractor
 from functools import reduce
 from csv2nii import get_ctr_numcent
 
-all_features_name = []
-features_name_per_label = []
-
 def create_image_mask_example():
     array_image = np.zeros((32,32,32)) 
     array_mask = np.zeros((32,32,32))
@@ -24,32 +21,39 @@ def create_image_mask_example():
     return image, mask
 
 def write_header(voi_type, labels_voi, radiomics_dir, params_file):
-    global all_features_name, features_name_per_label
     extractor = featureextractor.RadiomicsFeatureExtractor(params_file)
     ex_image, ex_mask = create_image_mask_example()
     dict_features_values = extractor.execute(ex_image, ex_mask, label = 1)
     features_name_per_label = [x for x in dict_features_values if not x.startswith("diagnostics_")]
+    all_features_names = []
     for label in labels_voi:
-        all_features_name += [str(label) + "_" + x for x in features_name_per_label]
-    header = ["ctr", "numcent"] +  all_features_name
+        all_features_names += [str(label) + "_" + x for x in features_name_per_label]
+    header = ["ctr", "numcent"] +  all_features_names
     with open(radiomics_dir + "header.csv", "w") as header_file:
         header_writer = csv.writer(header_file, delimiter = ',')
         header_writer.writerow(header)
+    with open(radiomics_dir + "nbr_features_per_label", 'w') as f:
+        f.write(str(len(features_name_per_label)))
 
-def compute_radiomics(image_path, mask_path, voi_type, labels_voi, newdosi_filename, radiomics_dir, subdir, params_file):
-    extractor = featureextractor.RadiomicsFeatureExtractor(params_file)
+def compute_radiomics(image_path, mask_path, voi_type, labels_voi, newdosi_filename, radiomics_dir, subdir, params_file, nbr_features_per_label):
     ctr, numcent = get_ctr_numcent(newdosi_filename)
-    nbr_features_per_label = len(features_name_per_label)
     all_features_values = np.zeros(0)
-    for label in labels_voi:
-        try:
-            dict_features_values = extractor.execute(image_path, mask_path, label = label)
-        except ValueError as err:
-            print(f"Raised ValueError. Ignoring the label mask {label}.")
-            all_features_values = np.append(all_features_values, [np.nan] * nbr_features_per_label)
-            continue
-        label_features_values = [dict_features_values[x] for x in dict_features_values if not x.startswith("diagnostics_")]
-        all_features_values = np.append(all_features_values, label_features_values)
+    # If the images are empty
+    if os.path.getsize(image_path) == 0 and os.path.getsize(mask_path) == 0:
+        print(f"{newdosi_filename}: empty nii images. Creating NaN values for the radiomics.")
+        all_features_values = [np.nan] * nbr_features_per_label * len(labels_voi)
+    # Else we compute the radiomics
+    else:
+        extractor = featureextractor.RadiomicsFeatureExtractor(params_file)
+        for label in labels_voi:
+            try:
+                dict_features_values = extractor.execute(image_path, mask_path, label = label)
+            except ValueError as err:
+                print(f"Raised ValueError. Ignoring the label mask {label}.")
+                all_features_values = np.append(all_features_values, [np.nan] * nbr_features_per_label)
+                continue
+            label_features_values = [dict_features_values[x] for x in dict_features_values if not x.startswith("diagnostics_")]
+            all_features_values = np.append(all_features_values, label_features_values)
     os.makedirs(radiomics_dir + subdir, exist_ok = True)
     with open(radiomics_dir + subdir + "/" + newdosi_filename + "_radiomics_" + voi_type + ".csv", "w") as radiomics_file:
         radiomics_writer = csv.writer(radiomics_file, delimiter = ',')
