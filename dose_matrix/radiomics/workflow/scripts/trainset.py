f4a7857439477d5ff92057a5a97d8385b1e87906
Mahmoud Bentriou <mahmoud.bentriou@centralesupelec.fr>
2021-12-02 14:18:48 +0100

    Add of clinical variables config

--- dose_matrix/radiomics/workflow/scripts/trainset.py
+++ dose_matrix/radiomics/workflow/scripts/trainset.py
@@ -18,7 +18,7 @@ def survival_date(row):
         min_date = datetime.strptime("31/12/2016", "%d/%m/%Y")
         return max([datetime.strptime(row[col], "%d/%m/%Y") for col in cols_date if not pd.isna(row[col])] + [min_date])
 
-def create_trainset(file_radiomics, file_fccss_clinical, analyzes_dir):
+def create_trainset(file_radiomics, file_fccss_clinical, analyzes_dir, clinical_variables):
     df_radiomics = pd.read_csv(file_radiomics)
     df_fccss = pd.read_csv(file_fccss_clinical, low_memory = False)
     cols_date = ["date_sortie", "datederm", "date_dvigr", "date_rep", "date_rep2", "cslt_date_cslt", "date_diag"]
@@ -29,8 +29,7 @@ def create_trainset(file_radiomics, file_fccss_clinical, analyzes_dir):
     df_fccss["survival_time_years"] = df_fccss[["survival_date", "datetime_date_diag"]].apply(lambda x: (x["survival_date"] - x["datetime_date_diag"]).total_seconds() / (365.25 * 24 * 3600), axis = 1)
     cols_radiomics = df_radiomics.columns.to_list()
     cols_radiomics.remove("ctr"), cols_radiomics.remove("numcent")
-    cols_clinical = []
-    df_trainset = df_fccss[["ctr", "numcent"] + ["Pathologie_cardiaque", "survival_time_years"] + cols_clinical]
+    df_trainset = df_fccss[["ctr", "numcent"] + ["Pathologie_cardiaque", "survival_time_years"] + clinical_variables]
     df_trainset = df_trainset.merge(df_radiomics, how = "left", on = ["ctr", "numcent"])
     df_trainset.to_csv(analyzes_dir + "trainset.csv")
 
