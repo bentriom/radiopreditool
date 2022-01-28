748f58e02ea2a064e43fe1cc567c404f64c3e6e1
Mahmoud Bentriou <mahmoud.bentriou@centralesupelec.fr>
2022-01-28 14:52:38 +0100

    fix awk file

--- dose_matrix/radiomics/workflow/scripts/awk_list_newdosi_files.sh
+++ dose_matrix/radiomics/workflow/scripts/awk_list_newdosi_files.sh
@@ -4,7 +4,7 @@ then
 fi
 echo 'ctr,numcent,localisation,id_treatment,filename_dose_matrix'
 #for dir in `ls -d */`
-for dir in "Curie/" "GR/" "Nice/" "Reims/" "Toulouse/"
+for dir in "CURIE/" "GR/" "NICE/" "REIMS/" "TOULOUSE/"
 do
     ls $dir | awk -v dir=$dir -F"_" '{print $2","substr($3,1,length($3)-1)","substr(dir,1,length(dir)-1)","substr($3,length($3),length($3))","$0}'
 done
